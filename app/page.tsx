54d36b85e51482228a6ff771742765640d627769
jdboachie <judeboachiekwame@gmail.com>
2024-01-16 22:38:18 +0000

    responsiveness update

--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,18 @@ export default function Page() {
       <div className="mt-4 md:p-0 py-10 rounded-lg md:bg-gray-50 md:dark:bg-gray-800 h-full flex">
         <div className="flex flex-col justify-center w-full items-center gap-6 px-6 py-10 md:px-20">
           <p className={`text-2xl md:w-2/3 text-black dark:text-gray-50 md:text-5xl md:leading-normal`}>
-            <strong className={`p-10 ${bricolage_grotesque.className}`}>Welcome to Alpha Invoices</strong>
-            <p className="text-sm py-5 px-10">
+            <strong className={`md:p-10 p-2 ${bricolage_grotesque.className}`}>Welcome to Alpha Invoices</strong>
+            <p className="text-sm py-5 md:px-10">
               When you have some text, how can you choose a typeface? The aim of this Google Fonts Knowledge module is to show that there are many considerations that can improve our type choices.
             </p>
           </p>
-          <div className='flex p-5 gap-6 max-sm:text-xs'>
-            <Link
+          <div className='flex flex-col md:flex-row p-5 gap-6 max-sm:text-xs'>
+            {/* <Link
               href="/login"
               className="cursor-not-allowed flex items-center gap-5 self-start rounded-lg bg-zinc-500 px-6 py-3 text-sm font-medium shadow-xl text-white transition-colors hover:bg-blue-400 md:text-base"
             >
               <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-            </Link>
+            </Link> */}
             <Link
               href="/dashboard"
               className=" flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium shadow-xl text-white transition-colors hover:bg-blue-400 md:text-base"
